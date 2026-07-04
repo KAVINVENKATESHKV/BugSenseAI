32ab374e0ac6cc7891120f2c0733f10176aacec8
Kavin Venkatesh <your-github-email@example.com>
2026-07-04 16:29:18 +0530

    Added PDF download button

--- app.py
+++ app.py
@@ -1,7 +1,10 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, send_file
 import google.generativeai as genai
 import os
 from dotenv import load_dotenv
+from reportlab.platypus import SimpleDocTemplate, Paragraph
+from reportlab.lib.styles import getSampleStyleSheet
+import io
 
 load_dotenv()
 
@@ -11,9 +14,11 @@ model = genai.GenerativeModel("gemini-2.5-flash")
 
 app = Flask(__name__)
 
+
 @app.route("/")
 def home():
     return render_template("index.html")
+
 
 @app.route("/predict", methods=["POST"])
 def predict():
@@ -49,6 +54,36 @@ Do NOT use:
         description=description,
         prediction=response.text
     )
+
+
+@app.route("/download")
+def download():
+
+    buffer = io.BytesIO()
+
+    doc = SimpleDocTemplate(buffer)
+
+    styles = getSampleStyleSheet()
+
+    story = []
+
+    story.append(Paragraph("<b>BugSense AI Report</b>", styles["Title"]))
+    story.append(Paragraph("This is a sample AI Bug Report.", styles["Normal"]))
+    story.append(Paragraph("Priority: High", styles["Normal"]))
+    story.append(Paragraph("Suggested Team: Backend Team", styles["Normal"]))
+    story.append(Paragraph("Estimated Fix Time: 2 Days", styles["Normal"]))
+
+    doc.build(story)
+
+    buffer.seek(0)
+
+    return send_file(
+        buffer,
+        as_attachment=True,
+        download_name="BugSenseAI_Report.pdf",
+        mimetype="application/pdf"
+    )
+
 
 if __name__ == "__main__":
     app.run(debug=True)
